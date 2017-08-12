9cb6984c01ac73293e13c5206e082ed1f029771b
Pranav <pranavm.13@gmail.com>
2017-08-12 11:20:14 -0700

    First commit for NN

---
+++ regression/neural_networks.py
@@ -0,0 +1,19 @@
+"""Regression of missing features using Multilayer Perceptron."""
+
+
+from sklearn.neural_network.multilayer_perceptron import MLPRegressor
+import pandas
+
+# Load data
+
+# Select important features
+
+# Filter data
+
+# Reformat data for sklearn
+
+# Create neural network object
+
+# Start training
+
+# Store results
