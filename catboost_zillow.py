6d972230ef127b2e79f64e5644418c95c85dd1ff
Pranav <pranavm.13@gmail.com>
2017-10-08 23:09:11 -0700

    Updated the hyperparameters

--- catboost_zillow.py
+++ catboost_zillow.py
@@ -86,12 +86,12 @@ test_df['transactiondate'] = pd.Timestamp('2016-12-01')  # Dummy
 test_df = add_date_features(test_df)
 X_test = test_df[train_features]
 print(X_test.shape)
-
+#
 # X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.5, random_state=42)
 #
-# param_grid = {'iterations': [150, 200, 250, 300],
-#               'learning_rate': [0.025, 0.03, 0.035, 0.04], 'depth': [5, 6, 7],
-#               'l2_leaf_reg': [2.5, 3, 3.5]}
+# param_grid = {'iterations': [330, 331],
+#               'learning_rate': [0.025], 'depth': [6],
+#               'l2_leaf_reg': [2.999, 3, 3.001]}
 #
 # params_log = []
 # scores_log = []
@@ -104,7 +104,8 @@ print(X_test.shape)
 #                 model = CatBoostRegressor(iterations=i, learning_rate=j,
 #                                           depth=k, l2_leaf_reg=l,
 #                                           loss_function='MAE',
-#                                           eval_metric='MAE', random_seed=1)
+#                                           eval_metric='MAE', random_seed=1,
+#                                           thread_count=8)
 #                 model.fit(X_train, y_train, cat_features=cat_feature_inds)
 #                 y_output = model.predict(X_test)
 #
@@ -114,7 +115,7 @@ print(X_test.shape)
 #                 ctr += 1
 #                 print "Score: ", score, "Counter: ", ctr
 #                 print "Params: ", i, j, k, l
-
+#
 # print params_log[scores_log.index(min(scores_log))]
 
 num_ensembles = 5
@@ -122,7 +123,7 @@ y_pred = 0.0
 for i in tqdm(range(num_ensembles)):
     # TODO(you): Use CV, tune hyperparameters
     model = CatBoostRegressor(
-        iterations=300, learning_rate=0.025,
+        iterations=330, learning_rate=0.025,
         depth=6, l2_leaf_reg=3,
         loss_function='MAE',
         eval_metric='MAE',
