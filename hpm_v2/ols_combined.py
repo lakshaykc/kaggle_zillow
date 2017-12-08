ec4f4f4f817a6f0e0384c9637b1a7b75605db203
lchauhan <lchauhan@umich.edu>
2017-12-08 11:56:25 -0500

    Organized the dir structure

--- hpm_v2/ols_combined.py
+++ hpm_v2/ols_combined.py
@@ -29,33 +29,53 @@ OLS_WEIGHT = 1 - catboost_weight
 np.random.seed(17)
 random.seed(17)
 
-train = pd.read_csv("./input/train_2017.csv", parse_dates=["transactiondate"])
-properties = pd.read_csv("./input/properties_2017.csv")
+train2016 = pd.read_csv("./input/train_2016_v2.csv", parse_dates=["transactiondate"])
+train2017 = pd.read_csv("./input/train_2017.csv", parse_dates=["transactiondate"])
+properties2016 = pd.read_csv("./input/properties_2016.csv")
+properties2017 = pd.read_csv("./input/properties_2017.csv")
 submission = pd.read_csv("./input/sample_submission.csv")
-print(len(train),len(properties),len(submission))
 
 def get_features(df):
     df["transactiondate"] = pd.to_datetime(df["transactiondate"])
     df["transactiondate_year"] = df["transactiondate"].dt.year
     df["transactiondate_month"] = df["transactiondate"].dt.month
     df['transactiondate'] = df['transactiondate'].dt.quarter
-    df = df.fillna(-999.0)
+    df = df.fillna(-1.0)
     return df
 
 def MAE(y, ypred):
     return np.sum([abs(y[i]-ypred[i]) for i in range(len(y))]) / len(y)
 
-train = pd.merge(train, properties, how='left', on='parcelid')
+print('Merge Train with Properties ...')
+train2016 = pd.merge(train2016, properties2016, how = 'left', on = 'parcelid')
+train2017 = pd.merge(train2017, properties2017, how = 'left', on = 'parcelid')
+
+print('Tax Features 2017  ...')
+train2017.iloc[:, train2017.columns.str.startswith('tax')] = np.nan
+train2017['structuretaxvaluedollarcnt'] = np.nan
+train2017['landtaxvaluedollarcnt'] = np.nan
+
+print('Concat Train 2016 & 2017 ...')
+train = pd.concat([train2016, train2017], axis = 0)
+test = pd.merge(submission[['ParcelId']], properties2016.rename(columns = {'parcelid': 'ParcelId'}), how = 'left', on = 'ParcelId')
+
+print ("Replacing NaN values by -1 !!")
+train.fillna(-1., inplace=True)
+test.fillna(-1., inplace=True)
+
 y = train['logerror'].values
-test = pd.merge(submission, properties, how='left', left_on='ParcelId', right_on='parcelid')
-properties = [] #memory
+
+properties2016 = [] #memory
+properties2017 = [] #memory
 
 exc = [train.columns[c] for c in range(len(train.columns)) if train.dtypes[c] == 'O'] + ['logerror','parcelid']
+exc_test = [train.columns[c] for c in range(len(train.columns)) if train.dtypes[c] == 'O'] + ['logerror','ParcelId','parcelid']
 col = [c for c in train.columns if c not in exc]
+col_test = [c for c in train.columns if c not in exc_test]
 
 train = get_features(train[col])
-test['transactiondate'] = '2017-01-01' #should use the most common training date
-test = get_features(test[col])
+test['transactiondate'] = '2016-01-01' #should use the most common training date
+test = get_features(test[col_test])
 
 reg = LinearRegression(n_jobs=-1)
 reg.fit(train, y); print('fit...')
@@ -75,7 +95,7 @@ test_columns = ['201610','201611','201612','201710','201711','201712']
 ##### COMBINE PREDICTIONS
 print( "\nCombining  predicitons ..." )
 
-ctbst = pd.read_csv("Only_CatBoost_17.csv")
+ctbst = pd.read_csv("Only_CatBoost.csv")
 pred0 = ctbst['201610'].values
 
 print( "\nCombined XGB/LGB/baseline predictions:" )
@@ -91,10 +111,8 @@ for i in range(len(test_dates)):
 print( "\nCombined XGB/LGB/baseline/OLS predictions:" )
 print( submission.head() )
 
-
-
 ##### WRITE THE RESULTS
 from datetime import datetime
 print( "\nWriting results to disk ..." )
-submission.to_csv('sub_ols_17_try.csv', index=False)
+submission.to_csv('sub_ols.csv', index=False)
 print( "\nFinished ...")
